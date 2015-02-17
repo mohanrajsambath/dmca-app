03323e5c6f7e4890ac1829a89c805cb4ecd5d941
stein <stein@salver.no>
2015-02-17 18:18:23 +0100

    Cosmetics

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller {
 
     /**
      * Where to redirect upon successful registration
-     * 
+     *
      * @var string
      */
     protected $redirectTo = 'notices/create';
